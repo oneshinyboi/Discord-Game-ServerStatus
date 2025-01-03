bf6f2a7752d7b9af23710e448f20194bf243ed3e
diamond <diamondalonzo@gmail.com>
2025-01-02 20:05:29 -0500

    changed user facing terminology from notify to ping

--- src/commands/NotifyAdmin.ts
+++ src/commands/NotifyAdmin.ts
@@ -2,8 +2,8 @@ import {PermissionFlagsBits, SlashCommandBuilder} from "discord.js";
 import {UpdateOrAddGuild} from "../storage/Db.js";
 
 export const NotifyAdminCommand = new SlashCommandBuilder()
-    .setName('notify-admin')
-    .setDescription('toggles notifying an admin if the server is down')
+    .setName('ping-admin')
+    .setDescription('toggles pinging an admin if the server is down')
     .addBooleanOption(option =>
         option
             .setName('enabled')
@@ -13,20 +13,20 @@ export const NotifyAdminCommand = new SlashCommandBuilder()
     .addMentionableOption(option =>
         option
             .setName('target')
-            .setDescription('The user or role to notify (required if enabling)')
+            .setDescription('The user or role to ping (required if enabling)')
             .setRequired(false)
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers);
 
 export async function interactionNotifyAdmin(interaction): Promise<void> {
-    if (interaction.commandName === 'notify-admin' && interaction.isChatInputCommand()) {
+    if (interaction.commandName === 'ping-admin' && interaction.isChatInputCommand()) {
         const enabled = interaction.options.getBoolean('enabled')
         const target = interaction.options.getMentionable('target');
 
         if (enabled) {
             if (!target) {
                 await interaction.reply({
-                    content: 'You must specify a user or role to notify when enabling notifications.',
+                    content: 'You must specify a user or role to ping when enabling pings.',
                     ephemeral: true,
                 });
                 return;
@@ -35,7 +35,7 @@ export async function interactionNotifyAdmin(interaction): Promise<void> {
                 await UpdateOrAddGuild(interaction.guildId, null, "@everyone", null)
 
                 await interaction.reply({
-                    content: `Notifications enabled! @everyone will be notified when the server is down.`,
+                    content: `Notifications enabled! @everyone will be pinged when the server is down.`,
                     ephemeral: true,
                 });
             }
@@ -43,7 +43,7 @@ export async function interactionNotifyAdmin(interaction): Promise<void> {
                 await UpdateOrAddGuild(interaction.guildId, null, target.id, null)
                 const targetType = target.user ? 'user' : 'role';
                 await interaction.reply({
-                    content: `Notifications enabled! The ${targetType} <@${target.id}> will be notified when the server is down.`,
+                    content: `Notifications enabled! The ${targetType} <@${target.id}> will be pinged when the server is down.`,
                     ephemeral: true,
                 });
             }
